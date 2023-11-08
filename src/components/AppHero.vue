df764a0ed6629f6f1e96d8f98ae813c23a125597
Hycarus <mirkomarasco08@gmail.com>
2023-11-08 15:10:09 +0100

    added bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img src="../../public/images/background.jpeg" alt="background">
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="css" scoped>
+    img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
